a6867fc7daac1100fb4c76e072d724b0cd851fa2
DancingRicardo <99092565+DancingRicardo@users.noreply.github.com>
2024-01-03 14:56:00 -0500

    Refactor to CLion and CMake, Compiles
    Fixed dependency on the Raylib Template and added CMake Support. currently built for CLion IDE. Compiles.

---
+++ include/AnimationHandler.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <cmath>
+#include <memory>
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+#include "raylib.h"
+
+class AnimationHandlerClass {
+private:
+
+	static constexpr int defaultFPS = 10;
+
+	double framesPerSecond = 5;
+
+	int pixelDensityPerChunk;
+
+	bool shouldUpdate;
+
+	bool isFrozen = false;
+
+	float timeSinceLastUpdate;
+
+	float currentFrame;
+
+	std::vector<std::shared_ptr<Texture2D>> textureVector;
+
+	Vector2 chunksPerSheet;
+
+	Vector2 currentChunk;
+
+	Vector2 directionsToFlow;
+
+public:
+
+	Texture2D testTexture = LoadTexture("resources/testspritesheet.png");
+
+	AnimationHandlerClass();
+
+	AnimationHandlerClass(std::string textureMapKeys);
+
+	std::shared_ptr<Texture2D> testTexturePtr;
+
+	//AnimationHandler(std::string textureMapKeys);
+
+	void update();
+
+	void freezeFrame();
+
+	void freezeFrame(Vector2 frameToFreezeOn);
+
+	void unfreezeFrame();
+
+	Vector2 spriteToUse();
+
+	Vector2 getCurrentChunk();
+
+	int getPixelDensityPerChunk();
+
+	void setCurrentChunk(Vector2 newchunk);
+
+	void setChunkMovement(Vector2 newDirectionsToFlow);
+
+
+
+};
---
+++ include/Platform.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "BaseClass.hpp"
+#include "raylib.h"
+#include <memory>
+
+class Platforms : BaseClass {
+private:
+
+	
+
+	Vector2 coords;
+	Vector2 movementVector;
+	bool hasCollision;
+	bool canMove;
+
+	uint16_t defaultWidth;
+	uint16_t defaultHeight;
+
+public: 
+
+	std::vector<Rectangle> platformVector;
+
+	Platforms();
+
+	Platforms(Rectangle recArg);
+
+	void update();
+
+	void reset();
+
+	void makeNewPlatform(Vector2 location);
+
+	void makeNewPlatform(Vector2 location, Vector2 dimensions);
+
+	void makeNewPlatform(Vector2 location, Vector2 dimensions, uint16_t amountToMake);
+
+	void removePlatform(uint32_t index);
+
+	const std::vector<Rectangle>& getPlatformVector();
+
+	
+
+
+
+};
